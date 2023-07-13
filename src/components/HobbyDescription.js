6fac4b38da24534129f3e4d4cc56dddbdbb10a59
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-07-13 16:32:05 +0200

    chore: update descriptions

--- src/components/HobbyDescription.js
+++ src/components/HobbyDescription.js
@@ -2,20 +2,20 @@ function HobbyDescription({ selectedHobby }) {
   const showHobbyDescription = (hobby) => {
     switch (hobby) {
       case "Chess":
-        return "I love Chess";
+        return "I am playing chess since 2021. My favourite opening is Sicilian Defense. I like watching when Hikaru Nakamura or Magnus Carlsen plays in top level tournaments.";
       case "Football":
-        return "I love Football";
+        return "I grown up playing football with my brother and friends. When we were kids, we used to play everyday on local stadium. Now we are meeting only sometimes at weekend.";
       case "Gaming":
-        return "I love Gaming";
+        return "I like playing strategy games such as Europa Universalis IV or Sid Meier's Civilization V. I also play CS:GO and League of Legends a lot. My biggest achievement is reaching Global Elite rank in CS:GO in 2020.";
       case "Coding":
-        return "I love Coding";
+        return "I started coding in 2019 in school. I was fascinated with HTML at first lessons. Later, we had JavaScript that became my favourite language. In 2022 I was learning my first framework which was Angular. ";
       default:
         return;
     }
   };
 
   return (
-    <div className="max-w-12 h-36 border rounded-xl text-left break-words">
+    <div className="h-36 border rounded-xl text-left break-words">
       <p className="text-lg">{showHobbyDescription(selectedHobby)}</p>
     </div>
   );
