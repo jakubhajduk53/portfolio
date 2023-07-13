09a54d646ff0c0e1051097dd8fd60d8407877e2d
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-07-13 16:38:28 +0200

    style: update About layout

--- src/components/About.js
+++ src/components/About.js
@@ -24,52 +24,57 @@ function About() {
   return (
     <div
       id="about"
-      className="flex flex-col items-center justify-center text-center gap-5 w-full pt-10"
+      className="flex flex-col items-center justify-center text-center gap-10 w-full"
     >
-      <div>
-        <p className="text-2xl">Hi, my name is Jacob</p>
-      </div>
-      <div>
-        <img
-          src={require("../assets/photo.jpg")}
-          alt="My photo"
-          className="w-36 border rounded-full"
-        />
-      </div>
-      <div>
-        <p className="text-2xl">I am {myAge} years old Frontend Developer</p>
-      </div>
-      <PiDiamondsFourBold />
-      <div className="flex flex-col gap-2 w-[70ch]">
-        <p className="text-2xl">My hobbies</p>
-        <div className="flex gap-2 justify-center">
-          <Hobby
-            value="Chess"
-            icon={FaChess}
-            isActive={checkIsActive("Chess")}
-            onClick={() => selectHobby("Chess")}
-          />
-          <Hobby
-            value="Football"
-            icon={BiFootball}
-            isActive={checkIsActive("Football")}
-            onClick={() => selectHobby("Football")}
-          />
-          <Hobby
-            value="Gaming"
-            icon={GrGamepad}
-            isActive={checkIsActive("Gaming")}
-            onClick={() => selectHobby("Gaming")}
-          />
-          <Hobby
-            value="Coding"
-            icon={BsCodeSquare}
-            isActive={checkIsActive("Coding")}
-            onClick={() => selectHobby("Coding")}
+      <div className="flex flex-col justify-center items-center pt-10 gap-5">
+        <div>
+          <p className="text-2xl">Hi, my name is Jacob</p>
+        </div>
+        <div>
+          <img
+            src={require("../assets/photo.jpg")}
+            alt="My photo"
+            className="w-36 border rounded-full"
           />
         </div>
-        <HobbyDescription selectedHobby={selectedHobby} />
+        <div>
+          <p className="text-2xl">I am {myAge} years old Frontend Developer</p>
+        </div>
       </div>
+      <PiDiamondsFourBold />
+      <div className="flex flex-col gap-5 w-[70ch]">
+        <p className="text-2xl">My hobbies</p>
+        <div className="flex flex-col gap-2">
+          <div className="flex gap-2 justify-center">
+            <Hobby
+              value="Chess"
+              icon={FaChess}
+              isActive={checkIsActive("Chess")}
+              onClick={() => selectHobby("Chess")}
+            />
+            <Hobby
+              value="Football"
+              icon={BiFootball}
+              isActive={checkIsActive("Football")}
+              onClick={() => selectHobby("Football")}
+            />
+            <Hobby
+              value="Gaming"
+              icon={GrGamepad}
+              isActive={checkIsActive("Gaming")}
+              onClick={() => selectHobby("Gaming")}
+            />
+            <Hobby
+              value="Coding"
+              icon={BsCodeSquare}
+              isActive={checkIsActive("Coding")}
+              onClick={() => selectHobby("Coding")}
+            />
+          </div>
+          <HobbyDescription selectedHobby={selectedHobby} />
+        </div>
+      </div>
+      <PiDiamondsFourBold />
     </div>
   );
 }
