f4a932095d67346346b5ae38070e347dda3b3129
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-07-14 18:38:59 +0200

    feat: update technologies

--- src/pages/Technologies.js
+++ src/pages/Technologies.js
@@ -1,13 +1,22 @@
 import Technology from "../components/Technology";
-import { FaReact } from "react-icons/fa";
 import {
   BiLogoRedux,
   BiLogoJavascript,
   BiLogoTailwindCss,
+  BiLogoAngular,
+  BiLogoBootstrap,
+  BiLogoGit,
+  BiLogoReact,
+  BiLogoHtml5,
+  BiLogoPhp,
+  BiLogoCss3,
+  BiLogoJquery,
+  BiLogoMongodb,
 } from "react-icons/bi";
-import { AiFillHtml5 } from "react-icons/ai";
+import { PiFileSqlDuotone } from "react-icons/pi";
+import { SiCsharp, SiCplusplus, SiXamarin, SiBulma } from "react-icons/si";
+import { FaWpforms } from "react-icons/fa";
 import { RiSupabaseFill } from "react-icons/ri";
-import { BsGit } from "react-icons/bs";
 
 function Technologies() {
   return (
@@ -19,11 +28,23 @@ function Technologies() {
         <div>
           <p className="text-2xl">Technologies I have learned</p>
         </div>
-        <div className="flex gap-5">
-          <Technology name="HTML" date="2019" icon={AiFillHtml5} />
+        <div className="flex gap-5 flex-wrap justify-center items-center">
+          <Technology name="HTML" date="2019" icon={BiLogoHtml5} />
+          <Technology name="CSS" date="2019" icon={BiLogoCss3} />
+          <Technology name="PHP" date="2019" icon={BiLogoPhp} />
+          <Technology name="SQL" date="2019" icon={PiFileSqlDuotone} />
+          <Technology name="C++" date="2019" icon={SiCplusplus} />
           <Technology name="JavaScript" date="2020" icon={BiLogoJavascript} />
-          <Technology name="Git" date="2022" icon={BsGit} />
-          <Technology name="React" date="2023" icon={FaReact} />
+          <Technology name="JQuery" date="2020" icon={BiLogoJquery} />
+          <Technology name="C#" date="2020" icon={SiCsharp} />
+          <Technology name="WPF Forms" date="2021" icon={FaWpforms} />
+          <Technology name="Xamarin" date="2021" icon={SiXamarin} />
+          <Technology name="Bootstrap" date="2022" icon={BiLogoBootstrap} />
+          <Technology name="Angular" date="2022" icon={BiLogoAngular} />
+          <Technology name="MongoDB" date="2022" icon={BiLogoMongodb} />
+          <Technology name="Git" date="2022" icon={BiLogoGit} />
+          <Technology name="React" date="2023" icon={BiLogoReact} />
+          <Technology name="Bulma" date="2023" icon={SiBulma} />
           <Technology
             name="Tailwind CSS"
             date="2023"
