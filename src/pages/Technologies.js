bb069277d6b33b9745feaeceff921d19f4b20b41
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-07-14 17:50:42 +0200

    feat: update Technologies

--- src/pages/Technologies.js
+++ src/pages/Technologies.js
@@ -1,7 +1,18 @@
+import Technology from "../components/Technology";
+import { FaReact } from "react-icons/fa";
+
 function Technologies() {
   return (
-    <div id="technologies" className="w-full  h-screen bg-red-100">
-      Technologies
+    <div
+      id="technologies"
+      className="flex flex-col items-center justify-center text-center gap-10 w-full h-screen  bg-red-50"
+    >
+      <div className="flex flex-col justify-center items-center pt-10 gap-5">
+        <div>
+          <p className="text-2xl">Technologies I have learned</p>
+        </div>
+        <Technology name="React" date="2023" icon={FaReact} />
+      </div>
     </div>
   );
 }
