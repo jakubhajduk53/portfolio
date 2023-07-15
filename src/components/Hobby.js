f0cbadc14b32a7ea3f8c758abff549e962d5f762
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-07-15 19:34:18 +0200

    style: fix Hobby

--- src/components/Hobby.js
+++ src/components/Hobby.js
@@ -2,7 +2,7 @@ import classNames from "classnames";
 
 function Hobby({ className, value, icon: Icon, isActive, onClick }) {
   const hobbyClasses = classNames(
-    "grid justify-items-center place-items-center border rounded-xl w-36 h-36 text-xl hover:bg-slate-200",
+    "grid justify-items-center place-items-center border rounded-xl w-16 h-16 sm:w-24 sm:h-24 md:w-36 md:h-36 sm:text-lg md:text-xl hover:bg-slate-200",
     isActive ? "bg-slate-200" : null,
     className
   );
@@ -10,7 +10,7 @@ function Hobby({ className, value, icon: Icon, isActive, onClick }) {
   return (
     <div className={hobbyClasses} onClick={onClick} tabIndex={0}>
       <div className="grid justify-items-center gap-2">
-        {Icon && <Icon className="shrink-0 text-3xl" />}
+        {Icon && <Icon className="shrink-0 text-xl sm:text-2xl md:text-3xl" />}
         {value}
       </div>
     </div>
