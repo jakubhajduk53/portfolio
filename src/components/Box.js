37c8ca72cf245b76e96aa0bdb89b54f9b53ef6a1
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-07-16 20:00:53 +0200

    style: update Box component

--- src/components/Box.js
+++ src/components/Box.js
@@ -10,8 +10,10 @@ function Box({
   boxType,
 }) {
   const boxClasses = classNames(
-    "grid justify-items-center place-items-center border rounded-xl w-16 h-16 sm:w-24 sm:h-24 md:w-36 md:h-36 sm:text-lg md:text-xl",
-    boxType === "Hobby" ? "hover:bg-slate-200" : "bg-red-100 hover:bg-red-200",
+    "grid justify-items-center place-items-center border rounded-xl text-sm md:text-xl",
+    boxType === "Hobby"
+      ? "hover:bg-slate-200 w-16 h-16 sm:w-24 sm:h-24 md:w-36 md:h-36 sm:text-lg"
+      : "bg-yellow-100/20 hover:bg-yellow-100 w-20 h-32 sm:w-24 sm:h-32 md:w-36 md:h-36 sm:text-base",
     isActive ? "bg-slate-200" : null,
     className
   );
@@ -23,10 +25,10 @@ function Box({
       tabIndex={boxType === "Hobby" ? 0 : null}
     >
       <div className="grid justify-items-center gap-2">
-        {Icon && <Icon className="shrink-0 text-xl sm:text-2xl md:text-3xl" />}
+        {Icon && <Icon className="shrink-0 text-2xl sm:text-2xl md:text-3xl" />}
         {value}
         {boxType === "Technology" ? (
-          <p className="text-base sm:text-lg md:text-xl">Started in: {date}</p>
+          <p className=" sm:text-base md:text-xl">Started in: {date}</p>
         ) : null}
       </div>
     </div>
