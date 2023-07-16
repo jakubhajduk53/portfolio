d3b5119a78cb397c1f09131ae585f2afced840b3
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-07-16 20:00:38 +0200

    chore: update containers

--- src/pages/Technologies.js
+++ src/pages/Technologies.js
@@ -1,4 +1,3 @@
-import Technology from "../components/Technology";
 import {
   BiLogoRedux,
   BiLogoJavascript,
@@ -17,41 +16,110 @@ import { PiFileSqlDuotone } from "react-icons/pi";
 import { SiCsharp, SiCplusplus, SiXamarin, SiBulma } from "react-icons/si";
 import { FaWpforms } from "react-icons/fa";
 import { RiSupabaseFill } from "react-icons/ri";
+import Box from "../components/Box";
 
 function Technologies() {
   return (
     <div
       id="technologies"
-      className="flex flex-col items-center justify-center text-center gap-10 w-full h-screen  bg-red-50"
+      className="grid content-center w-full h-screen bg-yellow-50 text-red-600"
     >
-      <div className="flex flex-col justify-center items-center pt-10 gap-5">
+      <div className="grid pt-10 gap-5 text-center justify-items-center place-items-center">
         <div>
-          <p className="text-2xl">Technologies I have learned</p>
+          <p className="text-xl md:text-2xl mb-5">
+            Technologies I have learned
+          </p>
         </div>
-        <div className="flex gap-5 flex-wrap justify-center items-center">
-          <Technology name="HTML" date="2019" icon={BiLogoHtml5} />
-          <Technology name="CSS" date="2019" icon={BiLogoCss3} />
-          <Technology name="PHP" date="2019" icon={BiLogoPhp} />
-          <Technology name="SQL" date="2019" icon={PiFileSqlDuotone} />
-          <Technology name="C++" date="2019" icon={SiCplusplus} />
-          <Technology name="JavaScript" date="2020" icon={BiLogoJavascript} />
-          <Technology name="JQuery" date="2020" icon={BiLogoJquery} />
-          <Technology name="C#" date="2020" icon={SiCsharp} />
-          <Technology name="WPF Forms" date="2021" icon={FaWpforms} />
-          <Technology name="Xamarin" date="2021" icon={SiXamarin} />
-          <Technology name="Bootstrap" date="2022" icon={BiLogoBootstrap} />
-          <Technology name="Angular" date="2022" icon={BiLogoAngular} />
-          <Technology name="MongoDB" date="2022" icon={BiLogoMongodb} />
-          <Technology name="Git" date="2022" icon={BiLogoGit} />
-          <Technology name="React" date="2023" icon={BiLogoReact} />
-          <Technology name="Bulma" date="2023" icon={SiBulma} />
-          <Technology
-            name="Tailwind CSS"
+        <div className="grid grid-cols-4 md:grid-cols-5 lg:grid-cols-5 xl:grid-cols-6 gap-5 ">
+          <Box
+            value="HTML"
+            date="2019"
+            icon={BiLogoHtml5}
+            boxType="Technology"
+          />
+          <Box value="CSS" date="2019" icon={BiLogoCss3} boxType="Technology" />
+          <Box value="PHP" date="2019" icon={BiLogoPhp} boxType="Technology" />
+          <Box
+            value="SQL"
+            date="2019"
+            icon={PiFileSqlDuotone}
+            boxType="Technology"
+          />
+          <Box
+            value="C++"
+            date="2019"
+            icon={SiCplusplus}
+            boxType="Technology"
+          />
+          <Box
+            value="JavaScript"
+            date="2020"
+            icon={BiLogoJavascript}
+            boxType="Technology"
+          />
+          <Box
+            value="JQuery"
+            date="2020"
+            icon={BiLogoJquery}
+            boxType="Technology"
+          />
+          <Box value="C#" date="2020" icon={SiCsharp} boxType="Technology" />
+          <Box
+            value="WPF Forms"
+            date="2021"
+            icon={FaWpforms}
+            boxType="Technology"
+          />
+          <Box
+            value="Xamarin"
+            date="2021"
+            icon={SiXamarin}
+            boxType="Technology"
+          />
+          <Box
+            value="Bootstrap"
+            date="2022"
+            icon={BiLogoBootstrap}
+            boxType="Technology"
+          />
+          <Box
+            value="Angular"
+            date="2022"
+            icon={BiLogoAngular}
+            boxType="Technology"
+          />
+          <Box
+            value="MongoDB"
+            date="2022"
+            icon={BiLogoMongodb}
+            boxType="Technology"
+          />
+          <Box value="Git" date="2022" icon={BiLogoGit} boxType="Technology" />
+          <Box
+            value="React"
+            date="2023"
+            icon={BiLogoReact}
+            boxType="Technology"
+          />
+          <Box value="Bulma" date="2023" icon={SiBulma} boxType="Technology" />
+          <Box
+            value="Tailwind CSS"
             date="2023"
             icon={BiLogoTailwindCss}
+            boxType="Technology"
           />
-          <Technology name="Redux" date="2023" icon={BiLogoRedux} />
-          <Technology name="Supabase" date="2023" icon={RiSupabaseFill} />
+          <Box
+            value="Redux"
+            date="2023"
+            icon={BiLogoRedux}
+            boxType="Technology"
+          />
+          <Box
+            value="Supabase"
+            date="2023"
+            icon={RiSupabaseFill}
+            boxType="Technology"
+          />
         </div>
       </div>
     </div>
