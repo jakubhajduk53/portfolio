96a041280e793b1a845835c774a041474e88450b
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-07-18 18:11:17 +0200

    feat: update Contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,20 @@
+import { AiOutlinePhone, AiOutlineMail } from "react-icons/ai";
+
 function Contact() {
   return (
-    <div id="contact" className="w-full h-screen bg-blue-100">
-      Contact
+    <div
+      id="contact"
+      className="grid content-center justify-items-center w-full h-screen bg-blue-100"
+    >
+      <div className="grid justify-items-center">
+        <div className="text-2xl">Contact</div>
+        <div className="flex items-center">
+          <AiOutlinePhone /> 509 778 233
+        </div>
+        <div className="flex items-center">
+          <AiOutlineMail /> jakubhajduk53@gmail.com
+        </div>
+      </div>
     </div>
   );
 }
