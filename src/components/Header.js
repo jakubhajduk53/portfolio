925b486fcfd4d9fcf10bbcbabb87ed9b9cd152b3
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-07-18 18:27:08 +0200

    style: fix padding

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import { AiOutlineProject, AiOutlinePhone } from "react-icons/ai";
 
 function Header() {
   return (
-    <div className="flex justify-between w-full h-32 pb-3 sm:pb-0 sm:h-16 pl-3 z-10 bg-orange-50 sticky top-0">
+    <div className="flex justify-between w-full h-32 pb-3 sm:pb-0 sm:h-16 pr-3 z-10 bg-orange-50 sticky top-0">
       <div className="ml-5 text-2xl italic self-center">Jakub Hajduk</div>
       <div className="flex flex-col justify-between sm:flex-row gap-1 sm:gap-5">
         <Anchor href="#about" value="About" icon={BsCardHeading} />
