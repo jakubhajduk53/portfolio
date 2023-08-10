170146367f9d50cba9b4158406f484024a01914c
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-08-11 01:07:24 +0200

    style: update Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { headerDescriptions } from "../data/descriptions";
 
 function Header() {
   return (
-    <div className="flex justify-around w-full h-32 pb-3 sm:pb-0 sm:h-16 pr-3 z-10 bg-cornsilk sticky top-0">
+    <div className="flex justify-around w-full h-32 pb-3 sm:pb-0 sm:h-16 pr-3 z-10 bg-cornsilk sticky top-0 shadow-md">
       <div className="ml-5 text-3xl self-center select-none">Jakub Hajduk</div>
       <div className="flex flex-col justify-between sm:flex-row gap-1 sm:gap-5">
         {headerDescriptions.map((headerInfo) => {
