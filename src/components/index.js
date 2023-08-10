2dd1960661de5b9b318d18ddbd973d80cdf73c31
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-08-10 18:38:23 +0200

    chore: update components export

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Anchor from "./Anchor";
+import Box from "./Box";
+import Header from "./Header";
+import HobbyDescription from "./HobbyDescription";
+
+export { Anchor, Box, Header, HobbyDescription };
