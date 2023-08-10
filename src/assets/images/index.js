1c6cb22b4315ebc2a848987bae0982b2a2a16fcd
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-08-10 18:37:56 +0200

    chore: update photos

---
+++ src/assets/images/index.js
@@ -0,0 +1,23 @@
+import CarsApp1 from "./cars-app1.webp";
+import CarsApp2 from "./cars-app2.webp";
+import CarsApp3 from "./cars-app3.webp";
+import CarsApp4 from "./cars-app4.webp";
+import MealApp1 from "./meal-app1.webp";
+import MealApp2 from "./meal-app2.webp";
+import MealApp3 from "./meal-app3.webp";
+import MealApp4 from "./meal-app4.webp";
+import MealApp5 from "./meal-app5.webp";
+import MyPhoto from "./my-photo.webp";
+
+export {
+  CarsApp1,
+  CarsApp2,
+  CarsApp3,
+  CarsApp4,
+  MealApp1,
+  MealApp2,
+  MealApp3,
+  MealApp4,
+  MealApp5,
+  MyPhoto,
+};
