2f02df6deb51846e1eb8beb6c383ee71f2f50892
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-08-10 18:38:44 +0200

    chore: relocate data to different folder

---
+++ src/data/technologies/index.js
@@ -0,0 +1,117 @@
+import {
+  BiLogoRedux,
+  BiLogoJavascript,
+  BiLogoTailwindCss,
+  BiLogoAngular,
+  BiLogoBootstrap,
+  BiLogoGit,
+  BiLogoReact,
+  BiLogoHtml5,
+  BiLogoPhp,
+  BiLogoCss3,
+  BiLogoJquery,
+  BiLogoMongodb,
+  BiLogoTypescript,
+} from "react-icons/bi";
+import { PiFileSqlDuotone } from "react-icons/pi";
+import { SiCsharp, SiCplusplus, SiXamarin } from "react-icons/si";
+import { FaWpforms } from "react-icons/fa";
+import { RiSupabaseFill } from "react-icons/ri";
+
+export const LearnedTechnologies = [
+  {
+    value: "HTML",
+    date: "2019",
+    icon: BiLogoHtml5,
+  },
+  {
+    value: "CSS",
+    date: "2019",
+    icon: BiLogoCss3,
+  },
+  {
+    value: "PHP",
+    date: "2019",
+    icon: BiLogoPhp,
+  },
+  {
+    value: "SQL",
+    date: "2019",
+    icon: PiFileSqlDuotone,
+  },
+  {
+    value: "C++",
+    date: "2019",
+    icon: SiCplusplus,
+  },
+  {
+    value: "JavaScript",
+    date: "2020",
+    icon: BiLogoJavascript,
+  },
+  {
+    value: "JQuery",
+    date: "2020",
+    icon: BiLogoJquery,
+  },
+  {
+    value: "C#",
+    date: "2020",
+    icon: SiCsharp,
+  },
+  {
+    value: "WPF Forms",
+    date: "2021",
+    icon: FaWpforms,
+  },
+  {
+    value: "Xamarin",
+    date: "2021",
+    icon: SiXamarin,
+  },
+  {
+    value: "Bootstrap",
+    date: "2022",
+    icon: BiLogoBootstrap,
+  },
+  {
+    value: "Angular",
+    date: "2022",
+    icon: BiLogoAngular,
+  },
+  {
+    value: "TypeScript",
+    date: "2022",
+    icon: BiLogoTypescript,
+  },
+  {
+    value: "MongoDB",
+    date: "2022",
+    icon: BiLogoMongodb,
+  },
+  {
+    value: "Git",
+    date: "2022",
+    icon: BiLogoGit,
+  },
+  {
+    value: "React",
+    date: "2023",
+    icon: BiLogoReact,
+  },
+  {
+    value: "Tailwind CSS",
+    date: "2023",
+    icon: BiLogoTailwindCss,
+  },
+  {
+    value: "Redux",
+    date: "2023",
+    icon: BiLogoRedux,
+  },
+  {
+    value: "Supabase",
+    date: "2023",
+    icon: RiSupabaseFill,
+  },
+];
