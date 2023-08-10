64d22f38ec9c1b57a8d6933cf0373edb2d6c844c
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-08-10 18:39:39 +0200

    feat: shorten and optimise code

--- src/pages/Technologies.js
+++ src/pages/Technologies.js
@@ -1,22 +1,5 @@
-import {
-  BiLogoRedux,
-  BiLogoJavascript,
-  BiLogoTailwindCss,
-  BiLogoAngular,
-  BiLogoBootstrap,
-  BiLogoGit,
-  BiLogoReact,
-  BiLogoHtml5,
-  BiLogoPhp,
-  BiLogoCss3,
-  BiLogoJquery,
-  BiLogoMongodb,
-} from "react-icons/bi";
-import { PiFileSqlDuotone } from "react-icons/pi";
-import { SiCsharp, SiCplusplus, SiXamarin, SiBulma } from "react-icons/si";
-import { FaWpforms } from "react-icons/fa";
-import { RiSupabaseFill } from "react-icons/ri";
 import Box from "../components/Box";
+import { LearnedTechnologies } from "../data/technologies";
 
 function Technologies() {
   return (
@@ -31,95 +14,17 @@ function Technologies() {
           </p>
         </div>
         <div className="grid grid-cols-4 md:grid-cols-5 lg:grid-cols-5 xl:grid-cols-6 gap-5 ">
-          <Box
-            value="HTML"
-            date="2019"
-            icon={BiLogoHtml5}
-            boxType="Technology"
-          />
-          <Box value="CSS" date="2019" icon={BiLogoCss3} boxType="Technology" />
-          <Box value="PHP" date="2019" icon={BiLogoPhp} boxType="Technology" />
-          <Box
-            value="SQL"
-            date="2019"
-            icon={PiFileSqlDuotone}
-            boxType="Technology"
-          />
-          <Box
-            value="C++"
-            date="2019"
-            icon={SiCplusplus}
-            boxType="Technology"
-          />
-          <Box
-            value="JavaScript"
-            date="2020"
-            icon={BiLogoJavascript}
-            boxType="Technology"
-          />
-          <Box
-            value="JQuery"
-            date="2020"
-            icon={BiLogoJquery}
-            boxType="Technology"
-          />
-          <Box value="C#" date="2020" icon={SiCsharp} boxType="Technology" />
-          <Box
-            value="WPF Forms"
-            date="2021"
-            icon={FaWpforms}
-            boxType="Technology"
-          />
-          <Box
-            value="Xamarin"
-            date="2021"
-            icon={SiXamarin}
-            boxType="Technology"
-          />
-          <Box
-            value="Bootstrap"
-            date="2022"
-            icon={BiLogoBootstrap}
-            boxType="Technology"
-          />
-          <Box
-            value="Angular"
-            date="2022"
-            icon={BiLogoAngular}
-            boxType="Technology"
-          />
-          <Box
-            value="MongoDB"
-            date="2022"
-            icon={BiLogoMongodb}
-            boxType="Technology"
-          />
-          <Box value="Git" date="2022" icon={BiLogoGit} boxType="Technology" />
-          <Box
-            value="React"
-            date="2023"
-            icon={BiLogoReact}
-            boxType="Technology"
-          />
-          <Box value="Bulma" date="2023" icon={SiBulma} boxType="Technology" />
-          <Box
-            value="Tailwind CSS"
-            date="2023"
-            icon={BiLogoTailwindCss}
-            boxType="Technology"
-          />
-          <Box
-            value="Redux"
-            date="2023"
-            icon={BiLogoRedux}
-            boxType="Technology"
-          />
-          <Box
-            value="Supabase"
-            date="2023"
-            icon={RiSupabaseFill}
-            boxType="Technology"
-          />
+          {LearnedTechnologies.map((technology) => {
+            return (
+              <Box
+                key={technology.value}
+                value={technology.value}
+                date={technology.date}
+                icon={technology.icon}
+                boxType="Technology"
+              />
+            );
+          })}
         </div>
       </div>
     </div>
