bce18bc3b180f4d3b2a5276b771bacb9fce48c12
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-08-12 17:11:37 +0200

    style: update styling for small screens

--- src/components/Anchor.js
+++ src/components/Anchor.js
@@ -2,8 +2,8 @@ import classNames from "classnames";
 
 function Anchor({ className, href, value, icon: Icon }) {
   const anchorClasses = classNames(
-    "flex items-center justify-center gap-1",
-    "text-sm sm:text-lg md:text-xl md:hover:text-carrot-orange transition-colors select-none",
+    "flex items-center justify-center gap-1 w-[50%] sm:w-auto pt-1 pb-1",
+    "text-lg md:text-xl md:hover:text-carrot-orange transition-colors select-none",
     className
   );
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,11 @@ import { headerDescriptions } from "../data/descriptions";
 
 function Header() {
   return (
-    <div className="flex justify-around w-full h-32 pb-3 sm:pb-0 sm:h-16 pr-3 z-10 bg-cornsilk sticky top-0 shadow-md">
-      <div className="ml-5 text-3xl self-center select-none">Jakub Hajduk</div>
-      <div className="flex flex-col justify-between sm:flex-row gap-1 sm:gap-5">
+    <div className="flex flex-col sm:flex-row justify-center sm:justify-around w-full p-2 sm:p-0 sm:pr-3 sm:h-16 z-10 sticky top-0 bg-cornsilk shadow-md">
+      <div className="ml-5 text-2xl md:text-3xl self-center select-none">
+        Jakub Hajduk
+      </div>
+      <div className="flex flex-wrap justify-between sm:gap-5">
         {headerDescriptions.map((headerInfo) => {
           return (
             <Anchor
