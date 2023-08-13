5aeebd690ecc895f586c66f107871417522c3203
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-08-13 16:23:19 +0200

    style: update Box styles

--- src/components/Box.js
+++ src/components/Box.js
@@ -10,13 +10,16 @@ function Box({
   boxType,
 }) {
   const hobbyClasses = classNames(
-    "w-16 h-16 sm:w-24 sm:h-24 md:w-36 md:h-36 sm:text-lg cursor-pointer hover:bg-vanilla transition-colors border-vanilla"
+    "w-16 h-16 sm:w-24 sm:h-24 md:w-36 md:h-36 sm:text-lg",
+    "border-vanilla hover:bg-vanilla transition-colors cursor-pointer"
   );
   const technologyClasses = classNames(
-    "w-20 h-32 sm:w-24 sm:h-32 md:w-36 md:h-36 xl:w-48 xl:h-48 sm:text-base bg-dark-colombia-blue hover:bg-uranian-blue border-uranian-blue"
+    "w-20 h-32 sm:w-24 sm:h-32 md:w-36 md:h-36 xl:w-48 xl:h-48 sm:text-base",
+    "bg-dark-colombia-blue hover:bg-uranian-blue border-uranian-blue"
   );
   const boxClasses = classNames(
-    "grid justify-items-center place-items-center border rounded-xl text-sm md:text-xl shadow-md",
+    "grid justify-items-center place-items-center border rounded-xl shadow-md",
+    "text-sm md:text-xl",
     boxType === "Hobby" ? hobbyClasses : technologyClasses,
     isActive ? "bg-vanilla" : "bg-cornsilk",
     className
@@ -29,7 +32,7 @@ function Box({
       tabIndex={boxType === "Hobby" ? 0 : null}
     >
       <div className="grid justify-items-center gap-2 select-none">
-        {Icon && <Icon className="shrink-0 text-2xl sm:text-2xl md:text-3xl" />}
+        {Icon && <Icon className="shrink-0 text-3xl md:text-5xl" />}
         {value}
         {boxType === "Technology" ? (
           <p className=" sm:text-base md:text-xl">Started in: {date}</p>
