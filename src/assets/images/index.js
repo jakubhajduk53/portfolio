734ce1230c2996facbb421487f6cded6225ad68f
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-08-13 17:28:18 +0200

    feat: update images

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,23 +1,6 @@
-import CarsApp1 from "./cars-app1.webp";
-import CarsApp2 from "./cars-app2.webp";
-import CarsApp3 from "./cars-app3.webp";
-import CarsApp4 from "./cars-app4.webp";
-import MealApp1 from "./meal-app1.webp";
-import MealApp2 from "./meal-app2.webp";
-import MealApp3 from "./meal-app3.webp";
-import MealApp4 from "./meal-app4.webp";
-import MealApp5 from "./meal-app5.webp";
-import MyPhoto from "./my-photo.webp";
+import MyPhoto from "./my-photo.jpg";
+import CarsApp from "./cars-app.jpg";
+import MealApp from "./meal-app.jpg";
+import Portfolio from "./portfolio.jpg";
 
-export {
-  CarsApp1,
-  CarsApp2,
-  CarsApp3,
-  CarsApp4,
-  MealApp1,
-  MealApp2,
-  MealApp3,
-  MealApp4,
-  MealApp5,
-  MyPhoto,
-};
+export { MyPhoto, CarsApp, MealApp, Portfolio };
