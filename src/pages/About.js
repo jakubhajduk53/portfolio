b9352d3fa64f8be7a39448d57ba056e006230855
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-08-13 16:24:27 +0200

    style: update About styles

--- src/pages/About.js
+++ src/pages/About.js
@@ -18,27 +18,32 @@ function About() {
   };
 
   return (
-    <div id="about" className="grid w-full h-project-screen text-center">
+    <div
+      id="about"
+      className="grid w-full h-project-screen text-center overflow-y-scroll md:overflow-auto"
+    >
       <div className="grid justify-items-center">
         <div>
-          <p className="text-xl md:text-3xl mt-5 select-none">Hi! I'm Jacob</p>
+          <p className="text-2xl md:text-3xl sm:mt-5 select-none">
+            Hi! I'm Jacob
+          </p>
         </div>
         <div>
           <img
             src={MyPhoto}
             alt="Jakub Hajduk"
-            className="w-24 sm:w-32 md:w-48 rounded-md border-2 border-cornsilk shadow-md select-none"
+            className="w-36 sm:w-32 md:w-48 rounded-md border-2 border-cornsilk shadow-md select-none"
           />
         </div>
         <div>
           <p className="text-xl md:text-2xl select-none">
-            I am {myAge} years old Frontend Developer
+            I'm {myAge} years old Frontend Developer
           </p>
         </div>
       </div>
       <div className="grid justify-items-center place-content-center">
-        <p className="text-xl md:text-3xl mb-5 select-none">My hobbies</p>
-        <div className="grid gap-2 w-[50rem]">
+        <p className="text-2xl md:text-3xl mb-5 select-none">My hobbies</p>
+        <div className="grid gap-2 md:w-[50rem]">
           <div className="grid grid-cols-5 gap-2 place-items-center">
             {hobbyDescriptions.map((hobby) => {
               return (
