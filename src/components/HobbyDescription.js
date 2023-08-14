68a3797c0d621308273ff21155d467c847b30120
Jakub Hajduk <jakubhajduk53@gmail.com>
2023-08-14 16:49:55 +0200

    style: remove border from HobbyDescription

--- src/components/HobbyDescription.js
+++ src/components/HobbyDescription.js
@@ -1,6 +1,6 @@
 function HobbyDescription({ description }) {
   return (
-    <div className="h-56 border rounded-xl text-left break-words shadow-md select-none">
+    <div className="h-56 rounded-xl text-left break-words shadow-md select-none">
       <p className="text-sm sm:text-base md:text-xl p-4">{description}</p>
     </div>
   );
